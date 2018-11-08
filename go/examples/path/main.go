08b28b0bd9ae79226af70c74049a958b2a9c5584
rebel-l <l.gaubisch@googlemail.com>
2018-11-08 17:37:05 +0100

    added working directory

--- go/examples/path/main.go
+++ go/examples/path/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"errors"
 	"fmt"
+	"log"
 	"os"
 	"path"
 	"path/filepath"
@@ -11,6 +12,12 @@ import (
 func main() {
 	fmt.Printf("path - Base (/foo/bar/readme.txt): %s\n", path.Base("/foo/bar/readme.txt"))
 	fmt.Printf("filepath - Base (/foo/bar/readme.txt): %s\n", filepath.Base("/foo/bar/readme.txt"))
+
+	wd, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Working Directory: %s\n", wd)
 
 	var counter int
 	filepath.Walk(".", func(path string, info os.FileInfo, err error) error {
