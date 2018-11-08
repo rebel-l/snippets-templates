b6e689e6079092b392657541ae88dfd4a0897715
rebel-l <l.gaubisch@googlemail.com>
2018-11-08 17:09:16 +0100

    added examples working with path

---
+++ go/examples/path/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"path"
+	"path/filepath"
+)
+
+func main() {
+	fmt.Printf("path - Base (/foo/bar/readme.txt): %s\n", path.Base("/foo/bar/readme.txt"))
+	fmt.Printf("filepath - Base (/foo/bar/readme.txt): %s\n", filepath.Base("/foo/bar/readme.txt"))
+
+	var counter int
+	filepath.Walk(".", func(path string, info os.FileInfo, err error) error {
+		fmt.Printf("Walk: %s\n", path)
+		counter++
+		if counter >= 10 {
+			return errors.New("enough")
+		}
+		return nil
+	})
+}
